bca41e0ed5f53135119657ffc52d0413f01edaf2
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-03-08 20:59:55 +0530

    model

---
+++ services/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/ems',()=>{
+    console.log('monodb connecteed successfully');
+})
+
+const Employee = mongoose.model('Employee',{
+    id:String,
+    uname:String,
+    age:String,
+    desg:String,
+    salary:Number
+})
+
+module.exports = {
+    Employee
+}
